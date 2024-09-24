8925ff9d979e851912c1539e74ea67dffd47bfc6
Markus-2902 <ullrich.m04@htlwienwest.at>
2024-09-24 09:20:12 +0200

    first

---
+++ Client/src/pages/Welcome.vue
@@ -0,0 +1,23 @@
+<script setup></script>
+
+<template>
+  <div class="q-mx-xl column mt-10 items-center pg">
+    <div class="col text-h3 pt-3">POI Guestbook</div>
+ <div class="col text-h6">Willkommen in der Porsche Informatik</div>
+    <div class="col mt-7">
+      <button class="button bg-orange-300 rounded-lg px-6 py-2">Anmelden</button>
+    </div>
+    <div class="col mt-4">
+      <button class="button bg-orange-300 rounded-lg px-6 py-2">Als Gast fortführen</button>
+    </div>
+    <div class="col mt-4">
+      <button class="button bg-orange-300 rounded-lg px-6 py-2">Abmelden</button>
+    </div>
+
+  </div>
+</template>
+
+<style>
+#fullbody {
+}
+</style>
